39343f262b07c3c9e5081627cf22da5fec2e702e
hbull5 <harcourtbullv@gmail.com>
2020-05-20 21:02:42 -0400

    CRUD api complete

--- index.js
+++ index.js
@@ -17,32 +17,48 @@ db.connect((err) => {
 
 // get all todos
 app.get('/', (req, res) => {
-    const stmt = `SELECT title FROM todos`;
+    const stmt = `SELECT * FROM todos`;
     db.query(stmt, (err, result) => {
         if(err) throw err;
-        res.status(200).send(result);
+        res.status(200).json(result);
     })
 })
 
 // get a specific todo 
 app.get('/:id', (req, res) => {
-    const stmt = `SELECT title FROM todos WHERE id = ?`;
+    const stmt = `SELECT * FROM todos WHERE id = ?`;
     db.query(stmt, req.params.id, (err, result) => {
         if(err) throw err;
-        res.status(200).send(result);
+        res.status(200).json(result);
     })
 })
 
 // create a new todo
-app.post('/', (req, res) => {
-    
+app.post('/:title', (req, res) => {
+    const stmt = `INSERT INTO todos (title) VALUES(?)`;
+    db.query(stmt, req.params.title, (err, result) => {
+        if(err) throw err;
+        res.status(200).json({success: true});
+    })
 })
 
 // delete a todo
-
+app.delete('/:id', (req, res) => {
+    const stmt = `DELETE FROM todos WHERE id = ?`;
+    db.query(stmt, req.params.id, (err, result) => {
+        if(err) throw err;
+        res.status(200).json({success: true})
+    })
+})
 
 // update a todo
-
+app.put('/:id/:title', (req, res) => {
+    const stmt = `UPDATE todos SET title = ? WHERE id = ?`;
+    db.query(stmt, [req.params.title, req.params.id], (err, result) => {
+        if(err) throw err;
+        res.status(200).json({success: true})
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}...`);
